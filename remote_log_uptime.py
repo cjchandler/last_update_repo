31d50615337e2a136f54960f6c3374d28253f57a
Carl <carljosephchandler@gmail.com>
2025-06-09 16:33:53 -0300

    data_automatic

--- remote_log_uptime.py
+++ remote_log_uptime.py
@@ -8,14 +8,14 @@ def add_commit_push_through_git( last_backup_time,  backup_interval):
         
         try:
             
-            f = open("/home/carl/Git_Projects/last_update_repo/mini_inflate.txt", "w")
+            f = open("/home/cjchandler/Git_Projects/last_update_repo/mini_inflate.txt", "w")
             f.write(str(time.time()))
             f.close()
-            os.system('cd /home/carl/Git_Projects/last_update_repo \n git pull origin main --no-edit')
+            os.system('cd /home/cjchandler/Git_Projects/last_update_repo \n git pull origin main --no-edit')
 
-            os.system( 'cd /home/carl/Git_Projects/last_update_repo \n git add . \n  git commit -a -m "data_automatic" ')
+            os.system( 'cd /home/cjchandler/Git_Projects/last_update_repo \n git add . \n  git commit -a -m "data_automatic" ')
             
-            os.system('cd /home/carl/Git_Projects/last_update_repo \n git push origin main')
+            os.system('cd /home/cjchandler/Git_Projects/last_update_repo \n git push origin main')
             print("backup via git is done")
             
             backedup = time.time()
